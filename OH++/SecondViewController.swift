6b1967d4171009ba740d67a6c6c931258bd02cf7
Nate Baker <natebaker@Nates-Mac.local>
2019-03-04 19:13:01 -0500

    The setup for the app

---
+++ OH++/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  OH++
+//
+//  Created by Nate Baker on 3/4/19.
+//  Copyright © 2019 Nate Baker. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
